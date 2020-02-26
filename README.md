aece76899b37ab7507063a33f3df0b5b95cc3532
Ricardo Ortega <ricardo@debian-vb.debian-vb>
2020-02-26 14:51:32 -0600

    fixed README syntax highlight, folder typo and refactored code

--- README.md
+++ README.md
@@ -1,34 +1,90 @@
 # Redis session API example
 ## Build image
 docker build api -t rortegasps/redis-session:1.0.0
+docker push rortegasps/redis-session:1.0.0
 
 ## Deploy to Kubernetes
 ### Redis
+
+```bash
 kubectl apply -f kubernetes/redis-deployment.yml
 kubectl apply -f kubernetes/redis-svc.yml
+```
 
 ### API
+
+```bash
 kubectl apply -f kubernetes/session-api.yml
+```
 
 ## Test
-Get API IP
-export SESSION_API=$(kubectl get svc redis-session-api -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
+Get API IP
+```bash
+export SESSION_API=$(kubectl get svc session-api -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+```
+
+Get profile without sessionID
+```bash
 curl -X GET \
   http://"$SESSION_API"/profile
+```
 
+Login to get a sesionID
+```bash
+export SESSION_ID=$( \
+  curl -X POST \
+  http://"$SESSION_API"/login \
+  -H 'Content-Type: application/json' \
+  -d '{"username":"Hugo","password":"Hugo123"}' \
+  | jq -r '.sessionID')
+```
+
+Get profile with sessionID
+```bash
+curl -X GET \
+  http://"$SESSION_API"/profile \
+  -H 'SessionID: '"$SESSION_ID"''
+```
+  
+Refresh session
+```bash
+curl -X POST \
+  http://"$SESSION_API"/refresh \
+  -H 'SessionID: '"$SESSION_ID"''
+```
+
+Wait 2.5 minutes (Redis delete session after 2 minutes)
+```bash
+curl -X GET \
+  http://"$SESSION_API"/profile \
+  -H 'SessionID: '"$SESSION_ID"''
+```
+
+## Scale up
+```bash
+kubectl scale --replicas=10 deployment session-api
+```
+
+Validate origin of every request
+
+```bash
 curl -X POST \
   http://"$SESSION_API"/login \
   -H 'Content-Type: application/json' \
   -d '{"username":"Hugo","password":"Hugo123"}'
+```
 
-curl -X GET \
-  http://"$SESSION_API"/profile \
-  -H 'SessionID: d1762368-4ec2-453a-a77d-b11125ea4f14'
-  
+```bash
 curl -X POST \
-  http://"$SESSION_API"/refresh \
-  -H 'SessionID: d1762368-4ec2-453a-a77d-b11125ea4f14'
+  http://"$SESSION_API"/login \
+  -H 'Content-Type: application/json' \
+  -d '{"username":"Paco","password":"Paco123"}'
+```
 
-## Scale up
-kubectl scale --replicas=10 deployment session-api
+```bash
+curl -X POST \
+  http://"$SESSION_API"/login \
+  -H 'Content-Type: application/json' \
+  -d '{"username":"Luis","password":"Luis123"}'
+```
--- api/main.go
+++ api/main.go
@@ -33,15 +33,22 @@ type Session struct {
 }
 
 
+
 // Redis
 var cache redis.Conn
 
+
+
 func main() {
 	// Redis session cache
-	ca, err := redis.DialURL("redis://redis:6379")
+	redisHostname := getEnv("REDIS_HOSTNAME","localhost")
+	redisPort := getEnv("REDIS_PORT","6379")
+	redisURL := "redis://" + redisHostname + ":" + redisPort
+	fmt.Println(redisURL)
+	ca, err := redis.DialURL(redisURL)
 	if err != nil {
-		panic(err)
 		fmt.Println("Redis not found ...")	
+		panic(err)
 	}
 	cache = ca
 	// Resources
@@ -57,9 +64,7 @@ func main() {
 func Login(w http.ResponseWriter, r *http.Request) {
 	// Validate method
 	if r.Method != http.MethodPost {
-        w.Header().Set("Content-Type", "application/json")
-        w.WriteHeader(http.StatusBadRequest)
-        w.Write([]byte("{\"message\": \"Bad method\"}"))
+        ResponseMessage(w,http.StatusBadRequest,"Wrong HTTP method")
         return
 	}
 
@@ -67,9 +72,7 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	// Validate body
 	err := json.NewDecoder(r.Body).Decode(&creds)
 	if err != nil {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("{\"message\": \"Bad body\"}"))
+		ResponseMessage(w,http.StatusBadRequest,"Bad JSON body")
 		return
 	}
 
@@ -78,95 +81,43 @@ func Login(w http.ResponseWriter, r *http.Request) {
 
 	// Is password correct?
 	if !ok || realPassword != creds.Password {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusUnauthorized)
-		w.Write([]byte("{\"message\": \"Wrong user or password\"}"))
+		ResponseMessage(w,http.StatusUnauthorized,"Wrong user or password")
 		return
 	}
 
-	//Token expiration time
-	exp := time.Now().Add(180 * time.Second).Unix()
-
-	// Get Origin hostname
-	hostname, err := os.Hostname()
-	if err != nil {
-		hostname = "Error"
-	}
-
-	// Create a new session ID
-	i, err := uuid.NewV4()
-	if err != nil {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("{\"message\": \"Internal server error\"}"))
-		fmt.Println(err)	
-		return
-	}
-	sessionID := i.String()
-
-	// Save sessionID on cache with expiring time of 180 seconds
-	_, err = cache.Do("SETEX", sessionID, "180", creds.Username)
-	if err != nil {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("{\"message\": \"Internal server error\"}"))
-		fmt.Println(err)	
-		return
-	}
-
-	// Return sessionID
-	res := &Session{SessionID: sessionID, Expiration: exp, Origin: hostname}
-	response, err := json.Marshal(res)
-    if err != nil {
-        w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("{\"message\": \"Internal server error\"}"))
-		fmt.Println(err)	
-        return
-    }
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	w.Write(response)
+	// Return session
+	ResponseSession(w, creds.Username)
 	
 }
 
 func Profile(w http.ResponseWriter, r *http.Request) {
 	// Validate method
 	if r.Method != http.MethodGet {
-        w.Header().Set("Content-Type", "application/json")
-        w.WriteHeader(http.StatusBadRequest)
-        w.Write([]byte("{\"message\": \"Bad method\"}"))
+		ResponseMessage(w,http.StatusBadRequest,"Wrong HTTP method")
         return
 	}
 	// Retrieve session id from Header
 	sessionID := r.Header.Get("SessionID")
 	if sessionID == "" {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusUnauthorized)
-		w.Write([]byte("{\"message\": \"You need to login to get a SessionID\"}"))
+		ResponseMessage(w,http.StatusUnauthorized,"You need to login to get a SessionID")
 		return
 	}
 
 	// Verify if session id exists on Redis
 	redisResponse, err := cache.Do("GET", sessionID)
 	if err != nil {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("{\"message\": \"Internal server error\"}"))
+		ResponseMessage(w,http.StatusInternalServerError,"Internal Server Error")
 		fmt.Println(err)	
 		return
 	}
 	// If not exists then the session id has expired or user is not authenticated
 	if redisResponse == nil {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusUnauthorized)
-		w.Write([]byte("{\"message\": \"You need to login\"}"))
+		ResponseMessage(w,http.StatusUnauthorized,"You need to login to get a SessionID")
 		return
 	}
 	// Show user's profile
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("{\"message\": \"Hi %s!\"}", redisResponse)))
+	user := fmt.Sprintf("%s", redisResponse)
+	ResponseMessage(w,http.StatusOK,"Hi " + user + "!")
 }
 
 
@@ -181,57 +132,88 @@ func Refresh(w http.ResponseWriter, r *http.Request) {
 	// Retrieve session id from Header
 	sessionID := r.Header.Get("SessionID")
 	if sessionID == "" {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusUnauthorized)
-		w.Write([]byte("{\"message\": You need to login to get a SessionID}"))
+		ResponseMessage(w,http.StatusUnauthorized,"You need to login to get a SessionID")
 		return
 	}
 	// Validate if session exists
 	redisResponse, err := cache.Do("GET", sessionID)
 	if err != nil {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("{\"message\": \"Internal server error\"}"))
+		ResponseMessage(w,http.StatusInternalServerError,"Internal Server Error")
 		fmt.Println(err)	
 		return
 	}
 	if redisResponse == nil {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusUnauthorized)
-		w.Write([]byte("{\"message\": \"SessionID expired, please login\"}"))
+		ResponseMessage(w,http.StatusUnauthorized,"SessionID expired, please login")
 		return
 	}
-	// New sessionID
-	u, err := uuid.NewV4()
-	if err != nil {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("{\"message\": \"Internal server error\"}"))
-		fmt.Println(err)	
-		return
-	}
-	newSessionID := u.String()
-	// Save new session to Redis
-	_, err = cache.Do("SETEX", newSessionID, "180", fmt.Sprintf("%s",redisResponse))
-	if err != nil {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("{\"message\": \"Internal server error\"}"))
-		fmt.Println(err)	
-		return
-	}
+
+	// Return new session
+	ResponseSession(w, fmt.Sprintf("%s",redisResponse))
+
 	// Delete old session from Redis
 	_, err = cache.Do("DEL", sessionID)
 	if err != nil {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("{\"message\": \"Internal server error\"}"))
+		ResponseMessage(w,http.StatusInternalServerError,"Internal Server Error")
 		fmt.Println(err)	
 		return
 	}
-	// Return new sessionID
-	response := map[string]string{"SessionID": newSessionID}
+}
+
+
+
+
+func ResponseMessage(w http.ResponseWriter, code int, message string) {
+	w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(code)
+    w.Write([]byte("{\"message\": \"" + message + "\"}"))
+
+}
+
+func ResponseSession(w http.ResponseWriter, username string) {
+	//Token expiration time
+	exp := time.Now().Add(120 * time.Second).Unix()
+
+	// Get Origin hostname
+	hostname, err := os.Hostname()
+	if err != nil {
+		hostname = "NoHostName"
+	}
+
+	// Create a new session ID
+	i, err := uuid.NewV4()
+	if err != nil {
+		ResponseMessage(w,http.StatusInternalServerError,"Internal Server Error")
+		fmt.Println(err)
+		return
+	}
+	sessionID := i.String()
+
+	// Save sessionID on cache with expiring time of 120 seconds
+	_, err = cache.Do("SETEX", sessionID, "120", username)
+	if err != nil {
+		ResponseMessage(w,http.StatusInternalServerError,"Internal Server Error")
+		fmt.Println(err)	
+		return
+	}
+
+	// Return sessionID
+	res := &Session{SessionID: sessionID, Expiration: exp, Origin: hostname}
+	response, err := json.Marshal(res)
+    if err != nil {
+    	ResponseMessage(w,http.StatusInternalServerError,"Internal Server Error")
+		fmt.Println(err)	
+        return
+    }
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(response)
+	w.Write(response)
+}
+
+
+func getEnv(key, defaultValue string) string {
+    value := os.Getenv(key)
+    if len(value) == 0 {
+        return defaultValue
+    }
+    return value
 }
